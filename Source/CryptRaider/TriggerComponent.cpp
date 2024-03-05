c8a107152ed0922d03d27db652cb960150ae030f
Moller_Matt_Laptop <136556009+MatiasMoller@users.noreply.github.com>
2024-03-05 13:16:00 +1100

    bit of polishing

---
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+
+
+UTriggerComponent::UTriggerComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    TArray<AActor*> Actors;
+    GetOverlappingActors(Actors);
+
+  for (int32 i = 0; i < Actors.Num(); i++)
+  {
+    FString ActorName = Actors[i]->GetActorNameOrLabel();
+    UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+  }
+  
+
+}
+
