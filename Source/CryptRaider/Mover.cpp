826d20a08e1d88f5263fcd40f335709244c43149
Moller_Matt_Laptop <136556009+MatiasMoller@users.noreply.github.com>
2024-03-06 20:45:50 +1100

    Final statue/puzzle working

--- Source/CryptRaider/Mover.cpp
+++ Source/CryptRaider/Mover.cpp
@@ -31,17 +31,18 @@ void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponent
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	FVector TargetLocation = OriginalLocation;
 	if (ShouldMove)
 	{
-		FVector CurrentLocation = GetOwner()->GetActorLocation();
-	FVector TargetLocation = OriginalLocation + MoveOffset;
-	float Speed = FVector::Distance(OriginalLocation, TargetLocation) / MoveTime;
+		TargetLocation = OriginalLocation + MoveOffset;
+	}
+	
+	FVector CurrentLocation = GetOwner()->GetActorLocation();
+	
+	float Speed = MoveOffset.Length() / MoveTime;
 
 	FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation,TargetLocation, DeltaTime, Speed);
 	GetOwner()->SetActorLocation(NewLocation);
-	}
-	
-	
 }
 
 void UMover::SetShouldMove(bool NewShouldMove)
